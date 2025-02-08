f95566a25dbf7049646b46fd5b53dd3ba549cc36
Altair-Bueno <altair.bueno@uma.es>
2025-02-08 13:08:41 +0100

    ref: Clean up Connect and Will structs by removing unnecessary whitespace

--- crates/sansio-mqtt5/src/types/connect.rs
+++ crates/sansio-mqtt5/src/types/connect.rs
@@ -1,29 +1,21 @@
 use super::*;
 
 #[derive(Debug, PartialEq, Clone)]
-
 pub struct Connect<'input> {
     pub protocol_name: MQTTString<'input>,
     pub protocol_version: u8,
     pub clean_start: bool,
-
     pub client_identifier: MQTTString<'input>,
-
     pub will: Option<Will<'input>>,
-
     pub user_name: Option<MQTTString<'input>>,
-
     pub password: Option<Cow<'input, [u8]>>,
     pub keep_alive: u16,
-
     pub properties: ConnectProperties<'input>,
 }
 #[derive(Debug, PartialEq, Clone)]
-
 pub struct ConnectHeaderFlags;
 
 #[derive(Debug, PartialEq, Clone)]
-
 pub struct Will<'input> {
     pub topic: PublishTopic<'input>,
     pub payload: Cow<'input, [u8]>,
@@ -33,23 +25,17 @@ pub struct Will<'input> {
 }
 
 #[derive(Debug, PartialEq, Clone, Default)]
-
 pub struct WillProperties<'input> {
     pub will_delay_interval: Option<u32>,
     pub payload_format_indicator: Option<FormatIndicator>,
     pub message_expiry_interval: Option<u32>,
-
     pub content_type: Option<MQTTString<'input>>,
-
     pub response_topic: Option<PublishTopic<'input>>,
-
     pub correlation_data: Option<Cow<'input, [u8]>>,
-
     pub user_properties: Vec<(MQTTString<'input>, MQTTString<'input>)>,
 }
 
 #[derive(Debug, PartialEq, Clone, Default)]
-
 pub struct ConnectProperties<'input> {
     pub session_expiry_interval: Option<u32>,
     pub receive_maximum: Option<NonZero<u16>>,
@@ -57,7 +43,6 @@ pub struct ConnectProperties<'input> {
     pub topic_alias_maximum: Option<u16>,
     pub request_response_information: Option<bool>,
     pub request_problem_information: Option<bool>,
-
     pub authentication: Option<AuthenticationKind<'input>>,
     pub user_properties: Vec<(MQTTString<'input>, MQTTString<'input>)>,
 }
