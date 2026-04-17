9322f87400b7f6a4b57a71672759e0be11ae8e66
Altair-Bueno <67512202+Altair-Bueno@users.noreply.github.com>
2026-04-17 16:57:58 +0200

    ref: Remove tokio code

--- crates/sansio-mqtt-v5-tokio/src/lib.rs
+++ crates/sansio-mqtt-v5-tokio/src/lib.rs
@@ -1,11 +1,1 @@
-#![forbid(unsafe_code)]
 
-mod driver;
-mod timer;
-mod transport;
-
-pub use driver::TokioClient;
-pub use sansio_mqtt_v5_contract::{
-    Action, ConnectOptions, PublishRequest, SubscribeRequest,
-};
-pub use sansio_mqtt_v5_types::Qos;
